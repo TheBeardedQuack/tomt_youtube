4f1e1e6d632301ccf2a3d1ef104e45091427f330
TheBeardedQuack <TheBeardedQuack@gmail.com>
2024-02-01 17:30:31 +0000

    Moved helper function to root as it may be useful elsewhere

--- src/lib.rs
+++ src/lib.rs
@@ -40,3 +40,41 @@ mod test
         Ok(())
     }
 }
+
+pub(crate) fn stringed_int<'de, D: serde::Deserializer<'de>>(
+    deserializer: D
+) -> Result<u64, D::Error> {
+    struct StringedInt;
+    
+    impl<'de> serde::de::Visitor<'de>
+    for StringedInt
+    {
+        type Value = u64;
+    
+        fn expecting(
+            &self,
+            formatter: &mut std::fmt::Formatter
+        ) -> std::fmt::Result {
+            write!(formatter, "An unsigned long, or an unsigned long wrapped as a string")
+        }
+    
+        fn visit_u64<E: serde::de::Error>(
+            self,
+            v: u64
+        ) -> Result<Self::Value, E> {
+            Ok(v)
+        }
+    
+        fn visit_str<E: serde::de::Error>(
+            self,
+            v: &str
+        ) -> Result<Self::Value, E> {
+            v.parse().map_err(|_e| serde::de::Error::invalid_value(
+                serde::de::Unexpected::Str(v),
+                &self
+            ))
+        }
+    }
+    
+    deserializer.deserialize_any(StringedInt)
+}
--- src/resources/channel/channel_statistics.rs
+++ src/resources/channel/channel_statistics.rs
@@ -15,13 +15,13 @@ pub struct ChannelStats
     #[serde(rename = "hiddenSubscriberCount")]
     pub hide_subs_count: bool,
 
-    #[serde(rename = "subscriberCount", deserialize_with = "stringed_int")]
+    #[serde(rename = "subscriberCount", deserialize_with = "crate::stringed_int")]
     pub subs_count: u64,
 
-    #[serde(rename = "videoCount", deserialize_with = "stringed_int")]
+    #[serde(rename = "videoCount", deserialize_with = "crate::stringed_int")]
     pub video_count: u64,
 
-    #[serde(rename = "viewCount", deserialize_with = "stringed_int")]
+    #[serde(rename = "viewCount", deserialize_with = "crate::stringed_int")]
     pub view_count: u64,
 }
 
@@ -31,42 +31,4 @@ for ChannelStats {
 
     const PART_NAME: &'static str = "statistic";
     const PART_KEY: PartKey = PartKey::Statistics;
-}
-
-fn stringed_int<'de, D: Deserializer<'de>>(
-    deserializer: D
-) -> Result<u64, D::Error> {
-    struct StringedInt;
-    
-    impl<'de> Visitor<'de>
-    for StringedInt
-    {
-        type Value = u64;
-    
-        fn expecting(
-            &self,
-            formatter: &mut std::fmt::Formatter
-        ) -> std::fmt::Result {
-            write!(formatter, "An unsigned long, or an unsigned long wrapped as a string")
-        }
-    
-        fn visit_u64<E: serde::de::Error>(
-            self,
-            v: u64
-        ) -> Result<Self::Value, E> {
-            Ok(v)
-        }
-    
-        fn visit_str<E: serde::de::Error>(
-            self,
-            v: &str
-        ) -> Result<Self::Value, E> {
-            v.parse().map_err(|_e| serde::de::Error::invalid_value(
-                serde::de::Unexpected::Str(v),
-                &self
-            ))
-        }
-    }
-    
-    deserializer.deserialize_any(StringedInt)
 }
