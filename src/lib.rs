33c62472bd205fa557b4b65b3fb77f54c325e319
TheBeardedQuack <TheBeardedQuack@gmail.com>
2024-02-01 16:49:21 +0000

    Added a test case for desired behaviour (required tokio to build)

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,39 @@ pub mod error;
 pub mod resources;
 
 pub(crate) type IdType = String;
+
+#[cfg(test)]
+mod test
+{
+    use crate::{
+        auth::YouTubeAuth,
+        client::YouTubeClient,
+        error::YtError,
+        resources::channel::ChannelId,
+    };
+
+    #[cfg(debug_assertions)]
+    #[tokio::test]
+    pub async fn channel_example()
+    -> Result<(), YtError> {
+        let client = YouTubeClient::new(
+            YouTubeAuth::ApiKey(env!("YT_API_KEY").to_string()),
+            None
+        ).expect("Failed to build client");
+        
+        let channel = client.channel(ChannelId::new("UC-uE71JAcOCAGbTaS2_C1BQ"));
+
+        let channel_data = channel.with_details()
+            .with_snippet()
+            .with_stats()
+            .fetch().await?;
+
+        println!("ChannelData = {channel_data:?}");
+
+        let snip = channel.snippet();
+        let snippet_data = snip.fetch().await?;
+
+        println!("SnippetData = {snippet_data:?}");
+        Ok(())
+    }
+}
