cb70a6087a1320535684dd6c202274397d934427
TheBeardedQuack <TheBeardedQuack@gmail.com>
2024-02-01 16:46:02 +0000

    Added Eq and new() to ChannelId

--- src/resources/channel/channel_id.rs
+++ src/resources/channel/channel_id.rs
@@ -3,10 +3,19 @@ use std::ops::Deref;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Default)]
-#[derive(Hash, PartialEq)]
+#[derive(Hash, PartialEq, Eq)]
 #[derive(Deserialize, Serialize)]
 #[serde(transparent)]
 pub struct ChannelId(crate::IdType);
+
+impl ChannelId
+{
+    pub fn new<S: AsRef<str>>(
+        id: S
+    ) -> Self {
+        Self(id.as_ref().to_string())
+    }
+}
 
 impl<T> AsRef<T>
 for ChannelId
