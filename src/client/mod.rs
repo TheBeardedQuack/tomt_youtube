5768b2240937755ef5e0755b3022685972c09a57
TheBeardedQuack <TheBeardedQuack@gmail.com>
2024-02-01 17:00:24 +0000

    Implemented fetch_channels to update any information required

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,74 +1,149 @@
 mod container;
-
-use std::{
-    sync::RwLock,
-    task::Poll,
-};
-
 pub(crate) use container::*;
+
+mod response;
+use response::*;
 
 use crate::{
     auth::YouTubeAuth,
-    error::{self, ResourceError, YtError},
+    error::{
+         ResourceError,
+         ResponseError,
+         YtError,
+         PANIC_LOCK_POISONED,
+    },
     resources::{
         channel::*,
+        channel_ref::*,
         Resource,
     }
 };
 
-use self::indirect::ChannelRef;
+use reqwest::{
+    header::{
+        self,
+        HeaderMap, HeaderValue
+    },
+    Url,
+};
+use std::{
+    sync::{Arc, RwLock},
+    task::{self, Poll},
+};
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct YouTubeClient
 {
     auth: YouTubeAuth,
-    channels: RwLock<RscContainer<ChannelData>>,
+    ht_client: reqwest::Client,
+
+    channels: Arc<RwLock<RscContainer<Channel>>>,
 }
 
 impl YouTubeClient
 {
+    const BASE_URL: &'static str = "https://www.googleapis.com/youtube/v3";
+    const PANIC_BAD_URL: &'static str = "Failed to parse compile time URL";
+
     pub fn new(
-        auth_mode: YouTubeAuth
-    ) -> Self {
-        Self {
+        auth_mode: YouTubeAuth,
+        ht_builder: Option<reqwest::ClientBuilder>
+    ) -> Result<Self, YtError> {
+        let client = ht_builder.unwrap_or_default()
+            .default_headers(HeaderMap::from_iter([
+                (header::ACCEPT, HeaderValue::from_static("application/json")),
+            ]))
+            .referer(true)
+            .https_only(true)
+            .build()?;
+
+        Ok(Self {
             auth: auth_mode,
+            ht_client: client,
+
             channels: Default::default(),
-        }
+        })
     }
 
     pub fn channel(
         &self,
         id: ChannelId
-    ) -> impl Channel + '_ {
+    ) -> ChannelRef {
         ChannelRef::new(self, id)
     }
 
     async fn fetch_channels(
         &self
-    ) -> Result<Vec<ChannelData>, YtError> {
-        let _result = Vec::<ChannelData>::new();
+    ) -> Result<ResponsePacket<Channel>, YtError> {
+        let (mut channels_lock, url) = {
+            let mut url = Url::parse(&format!(
+                "{}/{}",
+                Self::BASE_URL,
+                Channel::RSC_NAME
+            )).expect(Self::PANIC_BAD_URL);
+            let channels_lock = self.channels.write().expect(PANIC_LOCK_POISONED);
 
-        todo!("Issue HTTP request");
+            let (ids, parts) = {
+                let ids = channels_lock.pending_ids()
+                    .map(|s| s.as_str())
+                    .collect::<Vec<_>>()
+                    .join(",");
 
-        todo!("Parse data out of response");
+                let parts = channels_lock.pending_parts()
+                    .map(ToString::to_string)
+                    .collect::<Vec<_>>()
+                    .join(",");
 
-        todo!("Merge response data into existing data");
+                (ids, parts)
+            };
 
-        todo!("Return only the newly requested data");
+            url.query_pairs_mut()
+                .append_pair("maxResults", "50")
+                .append_pair("id", &ids)
+                .append_pair("part", &parts);
+        
+            match &self.auth {
+                YouTubeAuth::ApiKey(key) => url.query_pairs_mut().append_pair("key", key),
 
-        Ok(_result)
+                #[allow(unreachable_patterns)]
+                _ => todo!("Authentication method not yet supported"),
+            };
+
+            (channels_lock, url)
+        };
+
+        let text = self.ht_client.get(url)
+            .send().await?
+            .text().await?;
+
+        let page = match serde_json::from_str::<ResponsePacket<Channel>>(&text) {
+            Ok(page) => page,
+            Err(err) => Err(ResourceError::Response(
+                ResponseError::JsonDeserialize(err)
+            ))?,
+        };
+
+        // Merge response data into existing data
+        for item in page.items.iter() {
+            channels_lock.entry(item.id.clone())
+                .or_insert_with(|| Channel::with_id(item.id.clone()))
+                .update(item.clone())
+                .ok();
+        }
+
+        Ok(page)
     }
 }
 
-impl RscHolder<ChannelData>
+impl RscHolder<Channel>
 for YouTubeClient
 {
     fn touch(
         &self,
-        rsc_id: &<ChannelData as Resource>::Id,
-        rsc_part: <ChannelData as Resource>::PartKey
+        rsc_id: &<Channel as Resource>::Id,
+        rsc_part: <Channel as Resource>::PartKey
     ) -> &Self {
-        let mut lock = self.channels.write().expect(error::PANIC_LOCK_POISONED);
+        let mut lock = self.channels.write().expect(PANIC_LOCK_POISONED);
         lock.touch_id(rsc_id);
         lock.touch_part(rsc_part);
 
@@ -77,12 +152,13 @@ for YouTubeClient
 
     async fn fetch(
         &self,
-        rsc_id: &<ChannelData as Resource>::Id
-    ) -> Result<ChannelData, crate::error::YtError> {
+        rsc_id: &<Channel as Resource>::Id
+    ) -> Result<Channel, crate::error::YtError> {
         if let Some(channel) = self.get(rsc_id) {
             let current = channel.as_parts();
+            
             if self.channels.read()
-                .expect(error::PANIC_LOCK_POISONED)
+                .expect(PANIC_LOCK_POISONED)
                 .pending_parts()
                 .all(|p| current.contains(p))
             {
@@ -92,23 +168,33 @@ for YouTubeClient
             }
         }
 
-        { // Immediately yield to allow other ID's and params to be requested, then issue the actual request
-            // Touch resource so it gets refreshed when the next request NEEDS to go out
-            self.touch(rsc_id, ChannelParts::Id);
+        // Touch resource so it gets refreshed when the next request NEEDS to go out
+        self.touch(rsc_id, ChannelParts::Id);
 
-            let mut init = false;
-            std::future::poll_fn(move |_| {
-                match init {
-                    false => {
-                        init = true;
-                        Poll::Pending
-                    },
-                    true => Poll::Ready(())
+        { // Immediately yield to allow other ID's and params to be requested, then issue the actual request
+            struct Yield{yielded: bool}
+            impl std::future::Future for Yield{
+                type Output = ();
+
+                fn poll(
+                    mut self: std::pin::Pin<&mut Self>,
+                    cx: &mut task::Context<'_>
+                ) -> Poll<Self::Output> {
+                    match self.yielded {
+                        true => Poll::Ready(()),
+                        false => {
+                            self.yielded = true;
+                            cx.waker().wake_by_ref();
+                            Poll::Pending
+                        },
+                    }
                 }
-            })
+            }
+            Yield{yielded: false}
         }.await;
 
-        match self.fetch_channels().await?
+         match self.fetch_channels()
+            .await?.items
             .into_iter()
             .find(|ch| ch.id.eq(rsc_id))
         {
@@ -119,12 +205,12 @@ for YouTubeClient
 
     fn get(
         &self,
-        rsc_id: &<ChannelData as Resource>::Id
-    ) -> Option<<ChannelData as Resource>::Backing> {
-        self.touch(rsc_id, <ChannelData as Resource>::PartKey::Id);
+        rsc_id: &<Channel as Resource>::Id
+    ) -> Option<<Channel as Resource>::Backing> {
+        self.touch(rsc_id, <Channel as Resource>::PartKey::Id);
 
         self.channels.read()
-            .expect(error::PANIC_LOCK_POISONED)
+            .expect(PANIC_LOCK_POISONED)
             .iter()
             .find_map(|(k, v)| match rsc_id == k {
                 true => Some(v.clone()),
